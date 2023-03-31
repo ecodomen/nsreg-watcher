7d13e9f5733e73c9ae098d3e72e5b7fea57dfbbb
Maria Rezvova <rezvmaria@gmail.com>
2023-03-31 22:49:56 +0300

    Get .

--- nsreg/nsreg/spiders/nsreg_a100_py.py
+++ nsreg/nsreg/spiders/nsreg_a100_py.py
@@ -8,17 +8,17 @@ from nsreg.items import NsregItem
 REGEX_PATTERN = r"([0-9]*[.,\s])?[0-9]+\s*₽"
 
 class Nsreg2domainsSpider(scrapy.Spider):
-    name = 'nsreg_a100.py'
+    name = 'nsreg_a100'
     allowed_domains = ['a100.ru']
-    start_urls = ['http://a100.ru/']
+    start_urls = ['https://a100.ru/']
 
     def parse(self, response):
-        pricereg = response.xpath('//*[@id="overlappable"]/div/div/div/div[1]/div/div/div[2]/div/p[1]/text()')
+        pricereg = response.xpath('//*[@id="overlappable"]/div/div/div/div[1]/div/div/div[2]/div/p[1]/text()').get()
         if m := re.match(REGEX_PATTERN, str(pricereg)):
             pricereg = f'{float(pricereg)}'
             logging.info('pricereg = %s', pricereg)
         
-        priceprolong = response.xpath('//*[@id="overlappable"]/div/div/div/div[2]/div/div/div[2]/div/p[1]/text()')
+        priceprolong = response.xpath('//*[@id="overlappable"]/div/div/div/div[2]/div/div/div[2]/div/p[1]/text()').get()
         if m := re.match(REGEX_PATTERN, str(priceprolong)):
             priceprolong = f'{float(priceprolong)}'
             logging.info('priceprolong = %s', priceprolong)
