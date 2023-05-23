acc655f018433fd938e041049f3905feb48ee5f7
Maria Rezvova <rezvmaria@gmail.com>
2023-05-23 21:42:42 +0300

    Sample update.

--- src/grabber/nsreg/sample_update_regcomp.sql
+++ src/grabber/nsreg/sample_update_regcomp.sql
@@ -2,7 +2,7 @@ INSERT INTO regcomp (name, note1, note2, city, website, pricereg, priceprolong, 
 VALUES ('name', 'note1', 'note2', 'city', 'website', 'pricereg', 'priceprolong', 'pricechange') 
 ON CONFLICT (name) DO UPDATE
 SET ( note1, note2, city, website, pricereg, priceprolong, pricechange) =    (
-    COALESCE('note11', regcomp.note1),
+    COALESCE('note1', regcomp.note1),
     COALESCE('note2', regcomp.note2),
     COALESCE('city', regcomp.city),
     COALESCE('website', regcomp.website),
