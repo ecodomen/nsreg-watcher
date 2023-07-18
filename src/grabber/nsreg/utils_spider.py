4b874f39864ce3928fb9268803f2663a6abdc899
Rezvova Maria <rezvmaria@gmail.com>
2023-07-18 22:38:53 +0300

    Powerhosting, add  example sites to utils_spider

--- src/grabber/nsreg/utils_spider.py
+++ src/grabber/nsreg/utils_spider.py
@@ -14,6 +14,9 @@ EMPTY_PRICE = {
     'pricechange': None,
 }
 
+
+#Example site
+#https://www.powerhosting.ru/site/tariffs
 def moscow_tariffs(self, response, re_pattern, name):
     pricereg = response.xpath('/html/body/section/div/div/div/div[2]/div[1]/div[2]/span/text()').get()
     pricereg = find_price(re_pattern, pricereg)
@@ -35,6 +38,8 @@ def moscow_tariffs(self, response, re_pattern, name):
     return item
 
 
+#Example site
+#https://www.clickhost24.ru/price/
 def moscow_price(self, response, re_pattern, name):
     pricereg = response.xpath('/html/body/div[1]/div[3]/article/section/table[1]/tr/td[1]/article[1]/div/table/tr[5]/td[2]/text()').get()
     pricereg = find_price(re_pattern, pricereg)
@@ -55,7 +60,8 @@ def moscow_price(self, response, re_pattern, name):
 
     return item
 
-
+#Exaple site
+#https://clustered.ru/#price
 def moscow_rich_price(self, response, re_pattern, name):
     pricereg = response.xpath('/html/body/div[1]/div[4]/div/div[2]/div[3]/div/div/div/div[2]/div/div/div[1]/div/table/tbody/tr[2]/td[2]/div/p/text()').get()
     pricereg = find_price(re_pattern, pricereg)
