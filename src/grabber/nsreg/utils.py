7eae8239d39da95e37cc8532477775b6f4da684a
root <root@DESKTOP-DQDPJBS>
2023-08-13 15:26:31 +0300

    Restore rudimentary files utils.py and utils_spiders.py

---
+++ src/grabber/nsreg/utils.py
@@ -0,0 +1,36 @@
+#УДАЛИТЬ ПОСЛЕ РЕФАКТОРА СПАЙДЕРОВ
+import logging
+import re
+
+
+def find_price(re_pattern, price):
+    price = str(price).strip()
+    if price == "бесплатно":
+        price = 0
+    else:
+        if m := re.match(re_pattern, price):
+            price = m.group(1)
+    price = f'{float(price)}'
+    logging.info('price = %s', price)
+
+    return price
+
+
+def find_price_sub(re_pattern, price):
+    price = str(price).strip()
+    if m := re.match(re_pattern, price):
+        price = m.group(1)
+        price = re.sub(r'\s', '', price)
+        price = f'{float(price)}'
+        logging.info('price = %s', price)
+
+        return price
+
+
+def find_price_withoutre(price):
+    price = str(price)
+    price = re.sub(r'\s', '', price)
+    price = f'{float(price)}'
+    logging.info('price = %s', price)
+
+    return price
