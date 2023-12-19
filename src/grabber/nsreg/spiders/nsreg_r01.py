2613d5ddb4132b1753c365a785ae0938b8433240
Aleksey Vasilev <alexey.vasiljev@gmail.com>
2023-12-19 19:41:34 +0500

    Add BaseSpiderComponent parser for r01.ru

--- src/grabber/nsreg/spiders/nsreg_r01.py
+++ src/grabber/nsreg/spiders/nsreg_r01.py
@@ -1,40 +1,33 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from nsreg.items import NsregItem
 
-from ..utils import find_price_withoutre
-# работает
-EMPTY_PRICE = {
-    'price_reg': None,
-    'price_prolong': None,
-    'price_change': None,
-}
+from ..base_site_spider import BaseSpiderComponent
 
 
 class NsregR01Spider(scrapy.Spider):
-    name = 'nsreg_r01'
-    allowed_domains = ['r01.ru']
-    start_urls = ['https://r01.ru/domain/pay/']
+    name = "nsreg_r01.py"
+    start_urls = ["https://r01.ru/domain/pay/"]
+    allowed_domains = ("r01.ru")
+    site_names = ("ООО «Регистратор Р01»",)
 
+    # Конструктор класса
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Инициализация компонента BaseSpiderComponent с требуемыми параметрами
+        self.component = BaseSpiderComponent(
+            start_urls=self.start_urls,
+            allowed_domains=self.allowed_domains,
+            site_names=self.site_names,
+            regex=r"([0-9]+)[.,\s]?руб.*",
+            path={
+                'price_reg': '/html/body/table/tr[2]/td[2]/table/tr[4]/td[2]/text()',
+                'price_prolong': '/html/body/table/tr[2]/td[2]/table/tr[4]/td[3]/text()',
+                'price_change': '/html/body/table/tr[2]/td[2]/table/tr[4]/td[4]/text()'
+            }
+        )
+
+    # Метод для обработки ответов на запросы
     def parse(self, response):
-        price_reg = response.xpath(
-            '/html/body/table/tr[2]/td[2]/table/tr[4]/td[2]/text()').get()
-        price_reg = find_price_withoutre(price_reg)
+        # Применение метода parse компонента BaseSpiderComponent
+        return self.component.parse(response)
 
-        price_prolong = response.xpath(
-            '/html/body/table/tr[2]/td[2]/table/tr[4]/td[3]/text()').get()
-        price_prolong = find_price_withoutre(price_prolong)
-
-        price_change = response.xpath(
-            '/html/body/table/tr[2]/td[2]/table/tr[4]/td[4]/text()').get()
-        price_change = find_price_withoutre(price_change)
-
-        item = NsregItem()
-        item['name'] = "ООО «Регистратор Р01»"
-        price = item.get('price', EMPTY_PRICE)
-        price['price_reg'] = price_reg
-        price['price_prolong'] = price_prolong
-        price['price_change'] = price_change
-        item['price'] = price
-
-        yield item
