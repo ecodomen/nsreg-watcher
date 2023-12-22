37cca004490729bbddf9536512f2829c487ddbb6
yogorus <yogorus@gmail.com>
2023-12-22 16:22:48 +0300

    alter capnames parser

--- src/grabber/nsreg/spiders/nsreg_capnames.py
+++ src/grabber/nsreg/spiders/nsreg_capnames.py
@@ -1,46 +1,32 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-
-from nsreg.items import NsregItem
-
-from ..utils import find_price_sub
+from ..base_site_spider import BaseSpiderComponent
 
 REGEX_PATTERN = r".*([0-9]+\s[0-9]*)₽"
-EMPTY_PRICE = {
-    "price_reg": None,
-    "price_prolong": None,
-    "price_change": None,
-}
 
 
 class NsregCapnamesSpider(scrapy.Spider):
     name = "nsreg_capnames.py"
     start_urls = ["https://capnames.ru/"]
     allowed_domains = "capnames.ru"
+    site_names = ("ООО «КАПИТАЛЪ»",)
 
+    def __init__(self, name=None, **kwargs):
+        super().__init__(name, **kwargs)
+        self.component = BaseSpiderComponent(
+            start_urls=self.start_urls,
+            allowed_domains=self.allowed_domains,
+            site_names=self.site_names,
+            regex=REGEX_PATTERN,
+            path={
+                "price_reg": "/html/body/div[1]/div[2]/div/div/div[2]/div/div/div/div[1]/div/div/div[2]/div/p[1]/text()",
+                "price_prolong": "/html/body/div[1]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/p[1]/text()",
+                "price_change": "/html/body/div[1]/div[2]/div/div/div[2]/div/div/div/div[3]/div/div/div[2]/div/p[1]/text()",
+            },
+        )
+
+    # Метод для обработки ответов на запросы
     def parse(self, response):
-        price_reg = response.xpath(
-            "/html/body/div[1]/div[2]/div/div/div[2]/div/div/div/div[1]/div/div/div[2]/div/p[1]/text()"
-        )
-        price_reg = find_price_sub(REGEX_PATTERN, price_reg)
-
-        price_prolong = response.xpath(
-            "/html/body/div[1]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/p[1]/text()"
-        )
-        price_prolong = find_price_sub(REGEX_PATTERN, price_prolong)
-
-        price_change = response.xpath(
-            "/html/body/div[1]/div[2]/div/div/div[2]/div/div/div/div[3]/div/div/div[2]/div/p[1]/text()"
-        )
-        price_change = find_price_sub(REGEX_PATTERN, price_change)
-
-        item = NsregItem()
-        item["name"] = "ООО «КАПИТАЛЪ»"
-        price = item.get("price", EMPTY_PRICE)
-        price["price_reg"] = price_reg
-        price["price_prolong"] = price_prolong
-        price["price_change"] = price_change
-        item["price"] = price
-
-        yield item
+        # Применение метода parse компонента BaseSpiderComponent
+        return self.component.parse(response)
