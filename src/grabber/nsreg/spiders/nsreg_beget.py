a770c980a329a051b3162c51ef1629845a8ea2f2
Robert Solovev <rsolovye@gmail.com>
2023-12-25 16:21:04 +0000

    Добавил price_change (нашел ее на друго странице регистратора)

--- src/grabber/nsreg/spiders/nsreg_beget.py
+++ src/grabber/nsreg/spiders/nsreg_beget.py
@@ -3,20 +3,35 @@ import scrapy
 from nsreg.items import NsregItem
 
 from ..utils import find_price
-# работает, нет переноса, только с консультацией
+
+# работает, добавлен парс переноса с другой страницы. 
 
 REGEX_PATTERN = r"([0-9]{3,}).*"
+REGEX_CHANGE_PATTERN = r".*?(\d+)\s*рублей.*"
+
 EMPTY_PRICE = {
     'price_reg': None,
     'price_prolong': None,
     'price_change': None,
 }
 
-
 class NsregBegetSpider(scrapy.Spider):
     name = 'nsreg_beget'
     allowed_domains = ['beget.com']
     start_urls = ['https://beget.com/ru/domains/zone/ru']
+    
+    def parse_price_change(self, response):
+        price_change = response.xpath(
+            '//*[@id="__layout"]/div/div[3]/div[1]/div/div/div/div[3]/div[2]/div/div[1]/ul[3]/li[1]/text()').get()
+        price_change = find_price(REGEX_CHANGE_PATTERN, price_change)
+
+        item = NsregItem()
+        item['name'] = "ООО «Бегет»"
+        price = item.get('price', EMPTY_PRICE)
+        price['price_change'] = price_change
+        item['price'] = price
+        
+        yield item
 
     def parse(self, response):
         price_reg = response.xpath(
@@ -34,4 +49,4 @@ class NsregBegetSpider(scrapy.Spider):
         price['price_prolong'] = price_prolong
         item['price'] = price
 
-        yield item
+        yield scrapy.Request('https://beget.com/ru/kb/how-to/domains/kak-perenesti-domeny-v-beget', callback=self.parse_price_change)
