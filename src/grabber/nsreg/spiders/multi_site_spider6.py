d6d6a0fce9552d3d28f83d4a6b2cc17c73e48af7
Robert Solovev <rsolovye@gmail.com>
2023-12-25 10:24:22 +0000

    Добавил text() в path. Устранил ошибку конвертации значений с пробелами. Переписал функцию parse с использованием find_price_sub.

--- src/grabber/nsreg/spiders/multi_site_spider6.py
+++ src/grabber/nsreg/spiders/multi_site_spider6.py
@@ -9,7 +9,14 @@
 import scrapy
 
 from ..base_site_spider import BaseSpiderComponent
+from ..utils import find_price_sub
+from nsreg.items import NsregItem
 
+EMPTY_PRICE = {
+    'price_reg': None,
+    'price_prolong': None,
+    'price_change': None,
+}
 
 class MultiSiteSpider6(scrapy.Spider):
     name = 'multi_site_spider6'
@@ -40,13 +47,29 @@ class MultiSiteSpider6(scrapy.Spider):
             site_names=self.site_names,
             regex=r"([0-9]{1,3}(?:\s[0-9]{3})*[.,\s]?)?руб\.",
             path={
-                'price_reg': '//*[@id="post-66"]/div/div/figure/table/tbody/tr[1]/td[2]',
-                'price_prolong': '//*[@id="post-66"]/div/div/figure/table/tbody/tr[2]/td[2]',
-                'price_change': '//*[@id="post-66"]/div/div/figure/table/tbody/tr[3]/td[2]'
+                'price_reg': '//*[@id="post-66"]/div/div/figure/table/tbody/tr[1]/td[2]/text()',
+                'price_prolong': '//*[@id="post-66"]/div/div/figure/table/tbody/tr[2]/td[2]/text()',
+                'price_change': '//*[@id="post-66"]/div/div/figure/table/tbody/tr[3]/td[2]/text()'
             }
         )
 
-    # Метод для обработки ответов на запросы
+    # Parse с использованием функции find_price_sub
     def parse(self, response):
-        # Применение метода parse компонента BaseSpiderComponent
-        return self.component.parse(response)
+        price_reg = response.xpath(self.component.path['price_reg']).get()
+        price_reg = find_price_sub(self.component.regex['price_reg'], price_reg)
+
+        price_prolong = response.xpath(self.component.path['price_prolong']).get()
+        price_prolong = find_price_sub(self.component.regex['price_prolong'], price_prolong)
+
+        price_change = response.xpath(self.component.path['price_change']).get()
+        price_change = find_price_sub(self.component.regex['price_change'], price_change)
+
+        item = NsregItem()
+        item['name'] = self.site_names[self.start_urls.index(response.url)]
+        price = item.get('price', EMPTY_PRICE)
+        price['price_change'] = price_change
+        price['price_reg'] = price_reg
+        price['price_prolong'] = price_prolong
+        item['price'] = price
+
+        return item
