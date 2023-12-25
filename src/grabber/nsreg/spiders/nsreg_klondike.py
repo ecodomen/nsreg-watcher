ac2a4ce7d569be07fb1fcc62fe354ad930fa1715
Aleksey Vasilev <104360018+aleksey-vasilev@users.noreply.github.com>
2023-12-25 13:33:13 +0500

    Update nsreg_klondike.py with BaseSpiderComponent

--- src/grabber/nsreg/spiders/nsreg_klondike.py
+++ src/grabber/nsreg/spiders/nsreg_klondike.py
@@ -1,41 +1,28 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from nsreg.items import NsregItem
 
-from ..utils import find_price
-# работает
-REGEX_PATTERN = r"([0-9]+)[.,\s]?руб.*"
-EMPTY_PRICE = {
-    'price_reg': None,
-    'price_prolong': None,
-    'price_change': None,
-}
+from ..base_site_spider import BaseSpiderComponent
 
 
 class NsregKlondikeSpider(scrapy.Spider):
-    name = "nsreg_klondike"
-    allowed_domains = ["klondike-studio.ru"]
+    name = "nsreg_klondike.py"
     start_urls = ["https://klondike-studio.ru/domain/prices/"]
+    allowed_domains = ("klondike-studio.ru")
+    site_names = ("ООО «Клондайк Групп»",)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.component = BaseSpiderComponent(
+            start_urls=self.start_urls,
+            allowed_domains=self.allowed_domains,
+            site_names=self.site_names,
+            regex=r"([0-9]+)[.,\s]?руб*",
+            path={
+                'price_reg': '/html/body/div[2]/section/div/div/table/tbody/tr[1]/td[2]/text()',
+                'price_prolong': '/html/body/div[2]/section/div/div/table/tbody/tr[3]/td[2]/text()',
+                'price_change': '/html/body/div[2]/section/div/div/table/tbody/tr[5]/td[2]/text()'
+            }
+        )
 
     def parse(self, response):
-        price_reg = response.xpath(
-            '/html/body/div[2]/section/div/div/table/tbody/tr[1]/td[2]/text()').get()
-        price_reg = find_price(REGEX_PATTERN, price_reg)
-
-        price_prolong = response.xpath(
-            '/html/body/div[2]/section/div/div/table/tbody/tr[3]/td[2]/text()').get()
-        price_prolong = find_price(REGEX_PATTERN, price_prolong)
-
-        price_change = response.xpath(
-            '/html/body/div[2]/section/div/div/table/tbody/tr[5]/td[2]/text()').get()
-        price_change = find_price(REGEX_PATTERN, price_change)
-
-        item = NsregItem()
-        item['name'] = "ООО «Клондайк Групп»"
-        price = item.get('price', EMPTY_PRICE)
-        price['price_reg'] = price_reg
-        price['price_prolong'] = price_prolong
-        price['price_change'] = price_change
-        item['price'] = price
-
-        yield item
+        return self.component.parse(response)
