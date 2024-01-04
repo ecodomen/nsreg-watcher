859ff19a072ab136e4f2fe35cb9187ff5ad86e3d
rickgmv <volk1607sasa@yandex.ru>
2024-01-05 01:13:21 +0300

    Fix linter errrors

--- src/grabber/nsreg/spiders/nsreg_centralreg.py
+++ src/grabber/nsreg/spiders/nsreg_centralreg.py
@@ -13,11 +13,11 @@ class NsregCentralregSpider(scrapy.Spider):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.component = BaseSpiderComponent(
-            start_urls = self.start_urls,
-            allowed_domains = self.allowed_domains,
-            site_names = self.site_names,
-            regex = r"\s{3}([0-9]{2,3})",
-            path = { 
+            start_urls=self.start_urls,
+            allowed_domains=self.allowed_domains,
+            site_names=self.site_names,
+            regex=r"\s{3}([0-9]{2,3})",
+            path={
                 'price_reg': '/html/body/div[1]/div/div/div/div/div/div/div/div/div/div/article/div/div/div/div/div/table/tbody/tr[4]/td[2]/p/strong/text()',
                 'price_prolong': '/html/body/div[1]/div/div/div/div/div/div/div/div/div/div/article/div/div/div/div/div/table/tbody/tr[5]/td[2]/p/strong/text()',
                 'price_change': '/html/body/div[1]/div/div/div/div/div/div/div/div/div/div/article/div/div/div/div/div/table/tbody/tr[8]/td[2]/p/strong/text()'
