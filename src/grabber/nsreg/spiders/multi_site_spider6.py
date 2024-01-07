9e4e33300e372619c39acd8b685358cf55fac75b
Robert Solovyev <rsolovye@gmail.com>
2024-01-07 21:38:52 +0100

    Revert "Поправил ошибки линтера в 3 файлах"
    This reverts commit ace2ae9464a2259036712a82e2780b0ff3ee9000.

--- src/grabber/nsreg/spiders/multi_site_spider6.py
+++ src/grabber/nsreg/spiders/multi_site_spider6.py
@@ -18,19 +18,40 @@ EMPTY_PRICE = {
     'price_change': None,
 }
 
-
 class MultiSiteSpider6(scrapy.Spider):
     name = 'multi_site_spider6'
 
-    start_urls = ('https://clevereg.ru/tariffs/', 'https://targetnames.ru/tariffs/', 'https://technames.ru/tariffs/')
-    allowed_domains = ('https://clevereg.ru', 'https://targetnames.ru', 'https://technames.ru')
-    site_names = ('ООО «Клеверег»', 'ООО «Таргетнэймс»', 'ООО «Технэймс»')
+    start_urls = (
+        'https://clevereg.ru/tariffs/',
+        'https://targetnames.ru/tariffs/',
+        'https://technames.ru/tariffs/'
+    )
+    allowed_domains = (
+        'https://clevereg.ru',
+        'https://targetnames.ru',
+        'https://technames.ru'
+    )
+    site_names = (
+        'ООО «Клеверег»',
+        'ООО «Таргетнэймс»',
+        'ООО «Технэймс»'
+    )
 
     # Конструктор класса
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Инициализация компонента BaseSpiderComponent с требуемыми параметрами
-        self.component = BaseSpiderComponent(start_urls=self.start_urls, allowed_domains=self.allowed_domains, site_names=self.site_names, regex=r"([0-9]{1,3}(?:\s[0-9]{3})*[.,\s]?)?руб\.", path={'price_reg': '//*[@id="post-66"]/div/div/figure/table/tbody/tr[1]/td[2]/text()', 'price_prolong': '//*[@id="post-66"]/div/div/figure/table/tbody/tr[2]/td[2]/text()', 'price_change': '//*[@id="post-66"]/div/div/figure/table/tbody/tr[3]/td[2]/text()'})
+        self.component = BaseSpiderComponent(
+            start_urls=self.start_urls,
+            allowed_domains=self.allowed_domains,
+            site_names=self.site_names,
+            regex=r"([0-9]{1,3}(?:\s[0-9]{3})*[.,\s]?)?руб\.",
+            path={
+                'price_reg': '//*[@id="post-66"]/div/div/figure/table/tbody/tr[1]/td[2]/text()',
+                'price_prolong': '//*[@id="post-66"]/div/div/figure/table/tbody/tr[2]/td[2]/text()',
+                'price_change': '//*[@id="post-66"]/div/div/figure/table/tbody/tr[3]/td[2]/text()'
+            }
+        )
 
     # Parse с использованием функции find_price_sub
     def parse(self, response):
--- src/grabber/nsreg/spiders/nsreg_2domains.py
+++ src/grabber/nsreg/spiders/nsreg_2domains.py
@@ -3,7 +3,6 @@ import scrapy
 from nsreg.items import NsregItem
 
 from ..utils import find_price, find_price_withoutre
-
 # работает
 
 REGEX_PROLONG_PATTERN = r".*Продление\s+—\s+(([0-9]*[.,])?[0-9]+)\s+₽.*"
@@ -21,7 +20,8 @@ class Nsreg2domainsSpider(scrapy.Spider):
     start_urls = ['https://2domains.ru/domains']
 
     def parse_price_change(self, response):
-        price_change = response.xpath('/html/body/div/div[1]/section[1]/div/div/div/div/div[2]/div[2]/div/span/text()').get()
+        price_change = response.xpath(
+            '/html/body/div/div[1]/section[1]/div/div/div/div/div[2]/div[2]/div/span/text()').get()
         price_change = find_price(REGEX_CHANGE_PATTERN, price_change)
 
         item = NsregItem()
@@ -33,10 +33,12 @@ class Nsreg2domainsSpider(scrapy.Spider):
         yield item
 
     def parse(self, response):
-        price_reg = response.xpath('//*[@id="app"]/div[1]/section[3]/div/div[1]/div[1]/a/div[2]/text()').get()
+        price_reg = response.xpath(
+            '//*[@id="app"]/div[1]/section[3]/div/div[1]/div[1]/a/div[2]/text()').get()
         price_reg = find_price_withoutre(price_reg)
 
-        price_prolong = response.xpath('//*[@id="app"]/div[1]/section[3]/div/div[1]/div[1]/a/div[4]/text()').get()
+        price_prolong = response.xpath(
+            '//*[@id="app"]/div[1]/section[3]/div/div[1]/div[1]/a/div[4]/text()').get()
         price_prolong = find_price(REGEX_PROLONG_PATTERN, price_prolong)
 
         item = NsregItem()
@@ -45,5 +47,5 @@ class Nsreg2domainsSpider(scrapy.Spider):
         price['price_reg'] = price_reg
         price['price_prolong'] = price_prolong
         item['price'] = price
-
+        
         yield scrapy.Request('https://2domains.ru/domains/transfer', callback=self.parse_price_change)
