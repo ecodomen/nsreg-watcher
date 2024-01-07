2216fc929ffb8cfa05b63cb27d6047c4715f7fe9
Robert Solovyev <rsolovye@gmail.com>
2024-01-07 21:41:45 +0100

    Убрал из коммита multi_site_spider6.py чтобы не было конфликта

--- src/grabber/nsreg/spiders/nsreg_beget.py
+++ src/grabber/nsreg/spiders/nsreg_beget.py
@@ -4,7 +4,7 @@ from nsreg.items import NsregItem
 
 from ..utils import find_price
 
-# работает, добавлен парс переноса с другой страницы. 
+# работает, добавлен парс переноса с другой страницы.
 
 REGEX_PATTERN = r"([0-9]{3,}).*"
 REGEX_CHANGE_PATTERN = r".*?(\d+)\s*рублей.*"
@@ -15,14 +15,14 @@ EMPTY_PRICE = {
     'price_change': None,
 }
 
+
 class NsregBegetSpider(scrapy.Spider):
     name = 'nsreg_beget'
     allowed_domains = ['beget.com']
     start_urls = ['https://beget.com/ru/domains/zone/ru']
-    
+
     def parse_price_change(self, response):
-        price_change = response.xpath(
-            '//*[@id="__layout"]/div/div[3]/div[1]/div/div/div/div[3]/div[2]/div/div[1]/ul[3]/li[1]/text()').get()
+        price_change = response.xpath('//*[@id="__layout"]/div/div[3]/div[1]/div/div/div/div[3]/div[2]/div/div[1]/ul[3]/li[1]/text()').get()
         price_change = find_price(REGEX_CHANGE_PATTERN, price_change)
 
         item = NsregItem()
@@ -30,16 +30,14 @@ class NsregBegetSpider(scrapy.Spider):
         price = item.get('price', EMPTY_PRICE)
         price['price_change'] = price_change
         item['price'] = price
-        
+
         yield item
 
     def parse(self, response):
-        price_reg = response.xpath(
-            '//*[@id="__layout"]/div/div[2]/div/div[2]/div[1]/div/div[2]/div/div[1]/div/div[1]/p[2]/text()').get()
+        price_reg = response.xpath('//*[@id="__layout"]/div/div[2]/div/div[2]/div[1]/div/div[2]/div/div[1]/div/div[1]/p[2]/text()').get()
         price_reg = find_price(REGEX_PATTERN, price_reg)
 
-        price_prolong = response.xpath(
-            '//*[@id="__layout"]/div/div[2]/div/div[2]/div[1]/div/div[2]/div/div[1]/div/div[2]/p[2]/text()').get()
+        price_prolong = response.xpath('//*[@id="__layout"]/div/div[2]/div/div[2]/div[1]/div/div[2]/div/div[1]/div/div[2]/p[2]/text()').get()
         price_prolong = find_price(REGEX_PATTERN, price_prolong)
 
         item = NsregItem()
