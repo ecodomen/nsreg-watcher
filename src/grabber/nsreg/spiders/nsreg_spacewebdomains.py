ac06357888f75b3406671279a5e1cb1429d96172
Robert Solovyev <rsolovye@gmail.com>
2024-01-14 15:53:44 +0100

    Fix spider spacewebdomains.py

--- src/grabber/nsreg/spiders/nsreg_spacewebdomains.py
+++ src/grabber/nsreg/spiders/nsreg_spacewebdomains.py
@@ -4,10 +4,12 @@ from ..base_site_spider import BaseSpiderComponent
 
 
 class NsregSpacewebdomainsSpider(scrapy.Spider):
-    name = 'nsreg_spacewebdomains_spider'
+    name = 'nsreg_spacewebdomains'
 
     start_urls = ['https://spacewebdomains.ru/%D1%82%D0%B0%D1%80%D0%B8%D1%84%D1%8B/']
-    allowed_domains = 'spacewebdomains.ru'
+    allowed_domains = [
+        'spacewebdomains.ru',
+    ]
     site_names = ('ООО «СпейсВэб»',)
 
     # Конструктор класса
@@ -18,12 +20,12 @@ class NsregSpacewebdomainsSpider(scrapy.Spider):
             start_urls=self.start_urls,
             allowed_domains=self.allowed_domains,
             site_names=self.site_names,
-            # regex=r"([0-9]+)[.,\s]?руб.*",
+            regex=r"(\d+)",
             path={
-                'price_reg': '/html/body/div[1]/div/div[1]/main/article/div/div/figure/table/tbody/tr[2]/td[2]/text()',
-                'price_prolong': '/html/body/div[1]/div/div[1]/main/article/div/div/figure/table/tbody/tr[3]/td[2]/text()',
-                'price_change': '/html/body/div[1]/div/div[1]/main/article/div/div/figure/table/tbody/tr[4]/td[2]/text()'
-            }
+                'price_reg': 'translate(/html/body/div[1]/div/div[1]/main/article/div/div/figure/table/tbody/tr[2]/td[2]/text(), " ", "")',
+                'price_prolong': 'translate(/html/body/div[1]/div/div[1]/main/article/div/div/figure/table/tbody/tr[3]/td[2]/text(), " ", "")',
+                'price_change': 'translate(/html/body/div[1]/div/div[1]/main/article/div/div/figure/table/tbody/tr[4]/td[2]/text(), " ", "")',
+            },
         )
 
     # Метод для обработки ответов на запросы
