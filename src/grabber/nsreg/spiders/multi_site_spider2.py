ccab6dc778a1209213095bf65489862b94ff35a2
Robert Solovev <rsolovye@gmail.com>
2024-01-15 12:50:19 +0000

    Fixed multi_spider_2 for regdomains.ru

--- src/grabber/nsreg/spiders/multi_site_spider2.py
+++ src/grabber/nsreg/spiders/multi_site_spider2.py
@@ -1,9 +1,9 @@
 """
-Наименования сайтов: ООО «БЕСТРЕГ», ООО «БИГРЕГ», ООО «ВЕБРЕГ», ООО «ДОМЕНСЕРВИС», ООО «КЛИКРЕГ»,
+Наименования сайтов: ООО «ДОМЕНХОСТ», ООО «БЕСТРЕГ», ООО «БИГРЕГ», ООО «ВЕБРЕГ», ООО «ДОМЕНСЕРВИС», ООО «КЛИКРЕГ»,
 ООО «КЛИКХОСТ», ООО «Нетонлайн», ООО «Онлайнрег», ООО «ОПЕНРЕГ», ООО «ПРИМАХОСТ», ООО «РЕГ.РУ ДОМЕНЫ ХОСТИНГ»,
 ООО «Редрег», ООО «РЕНТЕР.РУ», ООО «ТЕЛЕБОРД», ООО «ТЕЛЕХОСТ», ООО «ТОПДОМЕН»
 
-Адреса сайтов: https://www.bestreg24.ru, https://www.bigreg24.ru, https://www.webreg24.ru,
+Адреса сайтов: https://www.regdomainhost.ru/, https://www.bestreg24.ru, https://www.bigreg24.ru, https://www.webreg24.ru,
 https://www.domenservice.ru, https://www.clickreg.ru, https://www.clickhost24.ru,
 https://www.neton-line.ru, https://www.online-reg.ru, https://www.open-reg.ru, https://www.prima-host.ru,
 http://www.regplanet.ru, https://www.red-reg.ru, https://www.sm-domains.ru, http://telebord24.ru, https://telehost24.ru,
@@ -20,6 +20,7 @@ class MultiSiteSpider2(scrapy.Spider):
     name = 'multi_site_spider2'
 
     start_urls = (
+        'https://www.regdomainhost.ru/price',
         'https://www.bestreg24.ru/price',
         'https://www.bigreg24.ru/price',
         'https://www.webreg24.ru/price',
@@ -37,6 +38,7 @@ class MultiSiteSpider2(scrapy.Spider):
         'http://topdomenreg24.ru/price'
     )
     allowed_domains = (
+        'https://www.regdomainhost.ru/',
         'https://www.bestreg24.ru',
         'https://www.bigreg24.ru',
         'https://www.webreg24.ru',
@@ -54,6 +56,7 @@ class MultiSiteSpider2(scrapy.Spider):
         'http://topdomenreg24.ru'
     ),
     site_names = (
+        'ООО «ДОМЕНХОСТ»',
         'ООО «БЕСТРЕГ»',
         'ООО «БИГРЕГ»',
         'ООО «ВЕБРЕГ»',
@@ -79,22 +82,21 @@ class MultiSiteSpider2(scrapy.Spider):
             start_urls=self.start_urls,
             allowed_domains=self.allowed_domains,
             site_names=self.site_names,
-            regex=r"([0-9]+[.,\s])?руб",
+            regex=r"(\d+)?руб",
             path={
-                'price_reg': (
-                    '/html/body/div[1]/div[3]/article/section/table[1]/tr/td[1]/article[1]/'
-                    'div/table/tr[5]/td[2]/text()'
+                'price_reg':        (
+                    'translate(/html/body/div[1]/div[3]/article/section/table[1]/tr/td[1]/article[1]/'
+                    'div/table/tr[5]/td[2]/text(), "\xa0 ", "")'
                 ),
                 'price_prolong': (
-                    '/html/body/div[1]/div[3]/article/section/table[1]/tr/td[1]/article[1]/'
-                    'div/table/tr[5]/td[3]/text()'
+                    'translate(/html/body/div[1]/div[3]/article/section/table[1]/tr/td[1]/article[1]/'
+                    'div/table/tr[5]/td[3]/text(), "\xa0 ", "")'
                 ),
                 'price_change': (
-                    '/html/body/div[1]/div[3]/article/section/table[2]/tr/td[1]/article[2]/'
-                    'div/table/tr[10]/td[2]/text()'
+                    'translate(/html/body/div[1]/div[3]/article/section/table[2]/tr/td[1]/article[2]/'
+                    'div/table/tr[10]/td[2]/text(), "\xa0 ", "")'
                 )
             }
-
         )
 
     # Метод для обработки ответов на запросы
