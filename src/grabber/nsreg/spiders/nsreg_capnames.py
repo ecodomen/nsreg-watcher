58cf8da83cd5ad29f923e768848bacb3ae5e2acf
yogorus <yogorus@gmail.com>
2024-01-16 17:32:46 +0300

    Wrap xpath into translate func, change regex

--- src/grabber/nsreg/spiders/nsreg_capnames.py
+++ src/grabber/nsreg/spiders/nsreg_capnames.py
@@ -2,7 +2,7 @@ import scrapy
 
 from ..base_site_spider import BaseSpiderComponent
 
-REGEX_PATTERN = r".*([0-9]+\s[0-9]*)₽"
+REGEX_PATTERN = r".*?([0-9]+)₽"
 
 
 class NsregCapnamesSpider(scrapy.Spider):
@@ -21,13 +21,19 @@ class NsregCapnamesSpider(scrapy.Spider):
             regex=REGEX_PATTERN,
             path={
                 "price_reg": (
-                    '/html/body/div[1]/div[2]/div/div/div[2]/div/div/div/div[1]/div/div/div[2]/div/p[1]/text()'
+                    'translate('
+                    '/html/body/div[1]/div[2]/div/div/div[2]/div/div/div/div[1]/div/div/div[2]/div/p[1]/text(), '
+                    '"\xa0 ", "")'
                     ),
                 "price_prolong": (
-                    '/html/body/div[1]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/p[1]/text()'
+                    'translate('
+                    '/html/body/div[1]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/p[1]/text(), '
+                    '"\xa0 ", "")'
                     ),
                 "price_change": (
-                    '/html/body/div[1]/div[2]/div/div/div[2]/div/div/div/div[3]/div/div/div[2]/div/p[1]/text()'
+                    'translate('
+                    '/html/body/div[1]/div[2]/div/div/div[2]/div/div/div/div[3]/div/div/div[2]/div/p[1]/text(), '
+                    '"\xa0 ", "")'
                     ),
             },
         )
