616e11870f80b540080962de07af395c2bb8f005
Robert Solovyev <rsolovye@gmail.com>
2024-01-17 14:06:13 +0100

    Whois bar, Domain Lookup

--- src/website/catalog/forms.py
+++ src/website/catalog/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.core.validators import RegexValidator
+
+URL_VALIDATOR_MESSAGE = 'Недопустимый URL.'
+URL_VALIDATOR = RegexValidator(regex=r'^(https?://)?(www\.)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(\/[a-zA-Z0-9._%+-]*)*$', message=URL_VALIDATOR_MESSAGE)
 
 SORT_BY_CHOICES = [
     ("CN", "Имя компании"),
@@ -28,3 +32,12 @@ class CompaniesSortForm(forms.Form):
         label="Поиск",
         max_length=50,
         required=False,)
+
+
+class DomainLookupForm(forms.Form):
+    domain = forms.CharField(
+        label="Домен:",
+        max_length=253,
+        required=False,
+        validators=[URL_VALIDATOR]
+        )
