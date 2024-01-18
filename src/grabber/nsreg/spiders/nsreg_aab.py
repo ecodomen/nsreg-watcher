c8782ab6c5caedac4ecc7841534c6bc9680a00da
Robert Solovev <rsolovye@gmail.com>
2024-01-18 10:12:24 +0000

    Переписал spider aab.ru с BaseSpiderComponent

--- src/grabber/nsreg/spiders/nsreg_aab.py
+++ src/grabber/nsreg/spiders/nsreg_aab.py
@@ -1,41 +1,29 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from nsreg.items import NsregItem
 
-from ..utils import find_price_withoutre
-# работает
-
-EMPTY_PRICE = {
-    'price_reg': None,
-    'price_prolong': None,
-    'price_change': None,
-}
+from ..base_site_spider import BaseSpiderComponent
 
 
 class Nsreg_aabSpider(scrapy.Spider):
     name = 'nsreg_aab'
     allowed_domains = ['aab.ru']
     start_urls = ['https://aab.ru/tarifi_na_uslugi.html']
+    site_names = ("ООО «ААБ Медиа»",)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.component = BaseSpiderComponent(
+            start_urls=self.start_urls,
+            allowed_domains=self.allowed_domains,
+            site_names=self.site_names,
+            regex=r".*?(\d+.\d+).*",
+            path={
+                'price_reg':        'translate(//*[@id="full_story"]/table/tbody/tr[3]/td[2]/text(), " ", "")',
+                'price_prolong':    'translate(//*[@id="full_story"]/table/tbody/tr[6]/td[2]/text(), " ", "")',
+                'price_change':     'translate(//*[@id="full_story"]/table/tbody/tr[9]/td[2]/text(), " ", "")'
+            }
+        )
 
     def parse(self, response):
-        price_reg = response.xpath(
-            '//*[@id="full_story"]/table/tbody/tr[3]/td[2]/text()').get()
-        price_reg = find_price_withoutre(price_reg)
-
-        price_prolong = response.xpath(
-            '//*[@id="full_story"]/table/tbody/tr[6]/td[2]/text()').get()
-        price_prolong = find_price_withoutre(price_prolong)
-
-        price_change = response.xpath(
-            '//*[@id="full_story"]/table/tbody/tr[9]/td[2]/text()').get()
-        price_change = find_price_withoutre(price_change)
-
-        item = NsregItem()
-        item['name'] = "ООО «ААБ Медиа»"
-        price = item.get('price', EMPTY_PRICE)
-        price['price_reg'] = price_reg
-        price['price_prolong'] = price_prolong
-        price['price_change'] = price_change
-        item['price'] = price
-
-        yield item
+        return self.component.parse(response)
