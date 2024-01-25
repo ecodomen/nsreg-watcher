6a3b4ed3bebb2f8ce5d81449170bf88a166dcb4f
Robert Solovev <rsolovye@gmail.com>
2024-01-25 10:19:04 +0100

    Restored formating

--- src/grabber/nsreg/models.py
+++ src/grabber/nsreg/models.py
@@ -21,14 +21,14 @@ class Registrator(models.Model):
     city = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"{self.id}: {self.name}"
+        return f'{self.id}: {self.name}'
 
     @staticmethod
     def get_registrator(id):
         return Registrator.objects.get(id=id)
 
     class Meta:
-        app_label = "catalog"
+        app_label = 'catalog'
 
 
 # class Domain(models.Model):
@@ -47,16 +47,16 @@ class ParseHistory(models.Model):
         return str(self.date)
 
     class Meta:
-        app_label = "catalog"
+        app_label = 'catalog'
 
 
 class Price(models.Model):
     DOMAINS = [
-        ("ru", "Ru"),
+        ('ru', 'Ru'),
     ]
     VALID_CHOICES = [
-        ("V", "valid"),
-        ("A", "absent"),
+        ('V', 'valid'),
+        ('A', 'absent'),
     ]
     id = models.BigAutoField(primary_key=True)
     registrator = models.ForeignKey(Registrator, on_delete=models.CASCADE)
@@ -65,30 +65,30 @@ class Price(models.Model):
     price_reg = models.DecimalField(max_digits=10, decimal_places=2)
     price_prolong = models.DecimalField(max_digits=10, decimal_places=2)
     price_change = models.DecimalField(max_digits=10, decimal_places=2)
-    reg_status = models.CharField("Статус", max_length=1, choices=VALID_CHOICES, default="V")
-    prolong_status = models.CharField("Статус", max_length=1, choices=VALID_CHOICES, default="V")
-    change_status = models.CharField("Статус", max_length=1, choices=VALID_CHOICES, default="V")
+    reg_status = models.CharField('Статус', max_length=1, choices=VALID_CHOICES, default='V')
+    prolong_status = models.CharField('Статус', max_length=1, choices=VALID_CHOICES, default='V')
+    change_status = models.CharField('Статус', max_length=1, choices=VALID_CHOICES, default='V')
 
     def __str__(self):
-        return f"{self.id}: {self.price_reg}, {self.price_prolong}, {self.price_change}"
+        return f'{self.id}: {self.price_reg}, {self.price_prolong}, {self.price_change}'
 
     class Meta:
-        app_label = "catalog"
+        app_label = 'catalog'
 
 
 class Parser(models.Model):
     id = models.BigAutoField(primary_key=True)
     registrator = models.ForeignKey(Registrator, on_delete=models.CASCADE)
-    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)  # Assuming the use of Django's default user model
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)  # Assuming the use of Django's default user model
     contributor_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     comment = models.TextField()
 
     def __str__(self):
-        return f"{self.id}: {self.contributor_name}"
+        return f'{self.id}: {self.contributor_name}'
 
     class Meta:
-        app_label = "catalog"
+        app_label = 'catalog'
 
 
 class ParseError(models.Model):
@@ -98,7 +98,7 @@ class ParseError(models.Model):
     message = models.TextField()
 
     def __str__(self):
-        return f"{self.id}: {self.message}"
+        return f'{self.id}: {self.message}'
 
     class Meta:
-        app_label = "catalog"
+        app_label = 'catalog'
