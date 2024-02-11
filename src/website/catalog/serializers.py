818fe6ab2149ec483bd688ab5f289561728e249f
yogorus <yogorus@gmail.com>
2024-02-11 15:13:26 +0300

    init drf conversion

---
+++ src/website/catalog/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+
+class RegistratorSerializer(serializers.Serializer):
+    pass
+    # id
+    # name
+    # nic_handle1
+    # nic_hanlde2
+    # website
+    # city
--- src/website/website/settings.py
+++ src/website/website/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'rest_framework'
 
     'catalog'
 ]
